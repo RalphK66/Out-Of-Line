4472935aad07cc34e3c8269bb30e23e09c553947
Ralph Kilian <rkilian1@my.bcit.ca>
2020-05-06 15:45:11 -0700

    added some logic to client login page

--- client/src/html-pages/login.js
+++ client/src/html-pages/login.js
@@ -1,51 +1,103 @@
 import React, { Component } from "react";
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
+import '../css/login.css'
 
-import { Form, FormGroup, Input, Button, Label, InputGroup, InputGroupAddon, InputGroupText } from "reactstrap";
+import {
+  Form,
+  FormGroup,
+  Input,
+  Button,
+  Label,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+} from "reactstrap";
 
 class Login extends Component {
-
-  constructor(props) {
-    super(props);
-
+  constructor() {
+    super();
     this.state = {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
+      errors: {},
     };
-
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
-
-    render () {
-  
-
-      return (
-        <div className="container col-sm-8 shadow" style={{marginTop: '50px', border: '20px solid #AAD2A9', borderRadius: '10px', padding: '20px'}}>
-          <Form onSubmit={this.onSubmit}>
-              <FormGroup>
-                  <div className="container shadow" style={{textAlign: 'center', padding: '20px', borderRadius: '10px'}}>
-                      <Label style={{color: "#6A6A6A"}} className="display-4">Login</Label>                      
-                      <InputGroup>
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText><FaUser/></InputGroupText>
-                        </InputGroupAddon>
-                        <Input type="username" name="username" id="Username" placeholder="username" bsSize="lg" onChange={this.handleChange}/> <br/>
-                      </InputGroup>
-                      <br/>
-                      <InputGroup>
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText><FaLock/></InputGroupText>
-                        </InputGroupAddon>
-                        <Input type="password" name="password" id="Password" placeholder="password" bsSize="lg" onChange={this.handleChange}/><br/>
-                      </InputGroup>
-                      <br/>
-                      <Button type="submit" style={{backgroundColor: '#AAD2A9', fontWeight: 'bolder', border: 'none', width: '150px'}} size="lg">Login</Button><br/>
-                  </div>
-              </FormGroup>
-          </Form>
-        </div>
-      );
+  onChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    const userData = {
+      username: this.state.username,
+      password: this.state.password,
     };
+    console.log(userData);
+    this.setState({
+      username: "",
+      password: ""
+    });
+  };
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div className="container col-sm-8 shadow box">
+        <Form noValidate onSubmit={this.onSubmit}>
+          <FormGroup>
+            <div className="container shadow form-box">
+              <Label className="display-4 login-label">
+                Login
+              </Label>
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <FaUser className="icon"/>
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  type="username"
+                  name="username"
+                  id="username"
+                  placeholder="username"
+                  bsSize="lg"
+                  onChange={this.onChange}
+                  value={this.state.username}
+                  error={errors.username}
+                />
+                <br />
+              </InputGroup>
+              <br />
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <FaLock className="icon"/>
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password"
+                  bsSize="lg"
+                  onChange={this.onChange}
+                  value={this.state.password}
+                  error={errors.password}/>
+                <br />
+              </InputGroup>
+              <br />
+              <Button
+                type="submit"
+                size="lg">Login</Button>
+              <br />
+            </div>
+          </FormGroup>
+        </Form>
+      </div>
+    );
   }
+}
 
 export default Login;

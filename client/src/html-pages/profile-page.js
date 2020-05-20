335228aaf227098b3e2bbbac6dd86a1eb151d938
Ralph Kilian <rkilian1@my.bcit.ca>
2020-05-20 16:54:03 -0700

    create profiel-page style sheet

--- client/src/html-pages/profile-page.js
+++ client/src/html-pages/profile-page.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Cookies from 'js-cookie';
+import { Container, Row, Col, Jumbotron } from 'reactstrap'
+import '../css/profile-page.css'
 
 class InQueue extends React.Component {
     constructor(props) {
@@ -55,11 +57,28 @@ class InQueue extends React.Component {
         this.displayInQueue();
 
         return(
-            <div className="container col-sm-8 shadow profile-page">
-                <p>Store Name: {this.state.storeName}</p>
-                <p>Queue Number: {this.state.queueNumber}</p>
-                <p>Wait Time: {this.state.waitTime}</p>
-            </div>
+          <Container className="col-sm-8 shadow profile-box">
+          <Row style={{height: "10vh"}}>
+              <Col>
+                  You're now in queue for:
+                  {this.state.storeName}
+              </Col>
+          
+          </Row> 
+          <Row style={{height: "50vh"}}>
+              <Col>
+                  <Jumbotron>
+                  <h1 style={{fontSize: "200px"}}>
+                  {this.state.queueNumber}
+                  </h1> 
+                  </Jumbotron>
+              </Col>
+           </Row> 
+      <Row style={{height: "10vh"}}>  
+ 
+          <p>Wait Time: {this.state.waitTime}</p>
+          </Row> 
+      </Container>
         );
     }
 }

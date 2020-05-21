6fef0c76a3b2e18c9396773369d45a1bb803803f
Ralph Kilian <rkilian1@my.bcit.ca>
2020-05-21 16:22:46 -0700

    changes to social media links

--- client/src/html-pages/social.js
+++ client/src/html-pages/social.js
@@ -16,9 +16,8 @@ import "../css/social.css";
 const url = "http://www.outofline.store";
 // Social Media URL's
 const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
-const twitterUrl = `https://twitter.com/intent/tweet?url=${url}`;
 const linkedinUrl = `https://www.linkedin.com/shareArticle?mini=true&url=${url}`;
-const whatsappUrl = `https://wa.me/whatsappphonenumber/?text=urlencodedtext`;
+const whatsappUrl = `https://wa.me/whatsappphonenumber/?text=I'm%20using%20Out%2Dof%2DLine%2D%2e%20`;
 
 const Social = () => {
   return (
@@ -26,9 +25,9 @@ const Social = () => {
       <Row>
         <Col className="social-media-columns">
           <TwitterShareButton
-            url={twitterUrl}
-            title="Out-of-Line"
-            hashtags={["OutOfLine", "StayinSafe"]}
+            url={url}
+            title={"Out-of-Line"}
+            hashtags={["outofline", "stayinsafe"]}
           >
             <TwitterIcon size={40} borderRadius="10" />
           </TwitterShareButton>
@@ -42,7 +41,9 @@ const Social = () => {
           <LinkedinShareButton
             url={linkedinUrl}
             title="Out-of-Line"
-            summary={`Out-of-Line is a free, web applicaion designed to increase the \n effictiveness of social distancing by means of a virtual queueue. \nShopping has never been safer. \nNeed to stand in line? Do it online, with Out-of-Line!`}
+            summary="Out-of-Line is a free, web application designed to increase the effectiveness of social distancing by means of a virtual queue. 
+            Shopping has never been safer. Need to stand in line? 
+            Do it online, with Out-of-Line!"
             source="www\w.outofline.store"
           >
             <LinkedinIcon size={40} borderRadius="10" />
